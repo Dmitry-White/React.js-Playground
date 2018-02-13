139034b581e87c7985fab2ec3715b0de1e6515b0
Dmitry <ecio1476@gmail.com>
2018-02-13 16:19:33 +0800

    Add some lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,19 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside Constructor', props);
+  };
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount');
+  };
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount');
+  };
+
   state = {
     persons: [
       { id:'ldkn', name: "Mike", age: "28" },
@@ -39,6 +52,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] Inside render');
     let persons = null;
 
     if (this.state.showPersons) {

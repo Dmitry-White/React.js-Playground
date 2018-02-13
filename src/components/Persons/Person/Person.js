108dc9b4d60f511f4f034f7eff4604860fa4642e
Dmitry <ecio1476@gmail.com>
2018-02-13 16:06:28 +0800

    Rewrite Person(s) to Statefull

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react'
 import classes from './Person.css';
 
-const person = props => {
-    const name = props.name;
-    const age = props.age;
-    const children = props.children;
+class Person extends Component {
+    render() {
+        return <div className={classes.Person}>
+            <p onClick={this.props.clicked}>
+                I'm a {this.props.name}  and I am {this.props.age} years old!
+            </p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        </div>
+    }
+}
 
-    return <div className={classes.Person}>
-        <p onClick={props.clicked}>I'm a {name}  and I am {age} years old!</p>
-        <p>{children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-};
-
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Person from './Person/Person';
 
-const persons = props => props.persons.map((person,index) => {
-    return <Person
-        key={person.id}
-        name={person.name}
-        age={person.age}
-        clicked={() => props.clicked(index)}
-        changed={(e) => props.changed(e,person.id)}
-    />;
-})
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person,index) => {
+            return <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                clicked={() => this.props.clicked(index)}
+                changed={(e) => this.props.changed(e,person.id)}
+            />;
+        })
+    }
+}
 
-export default persons;
+export default Persons;

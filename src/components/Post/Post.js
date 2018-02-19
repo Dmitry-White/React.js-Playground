f6772ba3f149a534f5fd8f8426b2052e5d0d2b48
Dmitry <ecio1476@gmail.com>
2018-02-19 10:16:42 +0800

    Enable css modules

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './Post.css';
+import classes from './Post.module.css';
 
-const post = (props) => (
-    <article className="Post">
+const post = props => (
+    <article className={classes.Post}>
         <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
+        <div className={classes.Info}>
+            <div className={classes.Author}>Author</div>
         </div>
     </article>
 );

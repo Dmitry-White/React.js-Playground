f7ec1e891a9bd9ff25c1208a151a97b48dc72838
Dmitry <ecio1476@gmail.com>
2018-02-19 11:26:52 +0800

    Add author field to each post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,13 +14,24 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Dmitry'
+                    }
+                })
+                this.setState({posts: updatedPosts});
             });
     };
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}/>
+            return <Post 
+                key={post.id} 
+                author={post.author}
+                title={post.title}
+            />
         });
 
         return (

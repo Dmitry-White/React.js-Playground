05570cba968d98901c401cd7118481a263bc1468
Dmitry <ecio1476@gmail.com>
2018-02-19 11:22:30 +0800

    Add fetching and showing posts from JSONPlaceholder

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,26 @@ import NewPost from '../../components/NewPost/NewPost';
 import classes from './Blog.module.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({posts: response.data});
+            });
+    };
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title}/>
+        });
+
         return (
             <div>
                 <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />

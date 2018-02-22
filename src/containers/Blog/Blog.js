1e89ea804374c4e4697cd4e03f3a116573e03f5c
Dmitry <ecio1476@gmail.com>
2018-02-22 10:49:54 +0800

    Restructure the project for routes usage

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,64 +1,21 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import classes from './Blog.module.css';
+import Posts from '../Posts/Posts'
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    };
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Dmitry'
-                    }
-                })
-                this.setState({posts: updatedPosts});
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
-    };
-
-    postSelectedHandler = id => {
-        this.setState({selectedPostId: id});
-    };
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    author={post.author}
-                    title={post.title}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            });
-        };
-
         return (
-            <div>
-                <section className={classes.Posts}>
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className={classes.Blog}>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts/>
             </div>
         );
     }

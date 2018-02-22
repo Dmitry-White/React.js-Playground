340a6913c7cfdfa6163c14ef36310dd82a04ef0c
Dmitry <ecio1476@gmail.com>
2018-02-22 11:02:20 +0800

    Prevent reloading with Link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import classes from './Blog.module.css';
-import Posts from '../Posts/Posts'
+import Posts from '../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -11,12 +12,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }

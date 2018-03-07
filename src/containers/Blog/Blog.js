f5e54deb40ddccc9dbf50219a14ffe3ddacd1a25
Dmitry <ecio1476@gmail.com>
2018-03-07 10:04:42 +0800

    Add async components load

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,8 +3,12 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import classes from './Blog.module.css';
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
 import FullPost from '../FullPost/FullPost';
+//import NewPost from '../NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+    return import('../NewPost/NewPost');
+});
 
 class Blog extends Component {
     render () {
@@ -31,8 +35,9 @@ class Blog extends Component {
                 </header>
                 <Switch>
                     <Route path="/" exact component={Posts}/>
-                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/new-post" component={AsyncNewPost}/>
                     <Route path="/:id" exact component={FullPost}/>
+                    <Route render={() => <h1>404 - Not Found!</h1>}/>
                 </Switch>
             </div>
         );

bbfd7cbc27a14b3e6ab0cc3d269a50e2c183d10d
Dmitry <ecio1476@gmail.com>
2018-04-14 23:57:47 +0800

    Add resolve function

--- schema/schema.js
+++ schema/schema.js
@@ -3,7 +3,8 @@ const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
 
 const users = [
@@ -25,10 +26,14 @@ const RootQuery = new GraphQLObjectType({
     field: {
         user: {
             type: UserType,
-            args: { id: { type: GraphQLString } }
+            args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
                 return _.find(users, { id: args.id });
             }
         }
     }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });

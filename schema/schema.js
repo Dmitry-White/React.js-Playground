bb96c034645b358bc47aa7ea0f8ac242796c8647
Dmitry <ecio1476@gmail.com>
2018-04-14 23:54:34 +0800

    Add root query

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,34 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt
 } = graphql;
 
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '47', firstName: 'Samantha', age: 21 },
+];
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     field: {
-        id: { type: GraphQLString},
-        firstName: { type: GraphQLString},
-        age: { type: GraphQLInt}
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    field: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } }
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
     }
 });

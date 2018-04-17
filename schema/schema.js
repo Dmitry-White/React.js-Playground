16d86f3e2153089d95a52522495db875c645d48c
Dmitry <ecio1476@gmail.com>
2018-04-17 12:59:44 +0800

    Add association with a company for each user

--- schema/schema.js
+++ schema/schema.js
@@ -7,14 +7,32 @@ const {
     GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString}
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(res => res.data);
+            }
+        }
     }
 });
+
+
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',

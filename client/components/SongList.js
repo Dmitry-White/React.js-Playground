94cf9e14e3b0ab3d4a9fb41d5f720cc9aeabce1c
Dmitry <ecio1476@gmail.com>
2018-04-18 14:04:56 +0800

    Add component and query connection

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>
                 Song List
@@ -19,4 +21,4 @@ const query = gql`
     }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);

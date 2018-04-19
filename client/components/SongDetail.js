07fa2f1a57cce4ab9df9efa799d5606a8170ba6f
Dmitry <ecio1476@gmail.com>
2018-04-19 15:27:18 +0800

    Add song details

---
+++ client/components/SongDetail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import fetchSong from '../queries/fetchSong';
+
+class SongDetail extends Component {
+    render() {
+        if (this.props.data.loading) {
+            return <div>Loading...</div>;
+        };
+        return (
+            <div>
+                <h3>{this.props.data.song.title}</h3>
+            </div>
+        );
+    };
+};
+
+export default graphql(fetchSong, {
+    options: (props) => ({variables: {id: props.params.id}})
+})(SongDetail);

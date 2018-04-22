4da97ce6531aaa1cb0819559eb6eac63d2f13a2c
Dmitry <ecio1476@gmail.com>
2018-04-22 22:14:36 +0800

    Initial boilerplate for Auth App

--- client/index.js
+++ client/index.js
@@ -1,34 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-
-import './style/style.css';
-import App from './components/App';
-import SongList from './components/Song/SongList';
-import SongCreate from './components/Song/SongCreate';
-import SongDetail from './components/Song/SongDetail';
-
-const client = new ApolloClient({
-    dataIdFromObject: o => o.id
-});
 
 const Root = () => {
   return (
-      <ApolloProvider client={client}>
-        <Router history={hashHistory}>
-            <Route path="/" component={App}>
-                <IndexRoute component={SongList} />
-                <Route path="/songs/new" component={SongCreate}/>
-                <Route path="/songs/:id" component={SongDetail}/>
-            </Route>
-        </Router>
-      </ApolloProvider>
+    <div>
+      Auth Starter
+    </div>
   );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
--- client/mutations/addSong.js
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-export default gql`
-    mutation AddSong($title: String){
-        addSong(title: $title) {
-            id
-        }
-    }
-`;

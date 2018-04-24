ea7a5c54975aa97f5480639aad7b34fc9a776677
Dmitry <ecio1476@gmail.com>
2018-04-24 14:51:40 +0800

    Add login mutation from the login form

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
 
+import Login from '../mutations/Login';
 import AuthForm from './AuthForm';
+import CurrentUser from '../queries/CurrentUser';
 
 class LoginForm extends Component {
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password },
+            refetchQueries: [{query: CurrentUser}]
+        });
+    };
+
     render() {
         return (
             <div>
                 <h3>Login</h3>
-                <AuthForm/>
+                <AuthForm onSubmit={this.onSubmit.bind(this)}/>
             </div>
         );
     };
 };
 
-export default LoginForm;
+export default graphql(Login)(LoginForm);

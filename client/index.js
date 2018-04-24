4e59641c8e6878b8dcce21d5783c5fca86eeb0b5
Dmitry <ecio1476@gmail.com>
2018-04-24 12:26:45 +0800

    Add Apollo Client

--- client/index.js
+++ client/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    dataIdFromObject: o => o.id
+});
 
 const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div>
+              Auth Starter
+            </div>
+        </ApolloProvider>
+    );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(
+    <Root />,
+    document.querySelector('#root')
+);

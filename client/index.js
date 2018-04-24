9da42088282a62a5e6fb1cf9a83a6d23fc9f58d5
Dmitry <ecio1476@gmail.com>
2018-04-24 14:17:20 +0800

    Add dull LoginForm

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 const networkInterface = createNetworkInterface({
     uri: '/graphql',
@@ -22,7 +23,9 @@ const Root = () => {
     return (
         <ApolloProvider client={client}>
             <Router history={hashHistory}>
-                <Router path="/" component={App}/>
+                <Route path="/" component={App}>
+                    <Route path="/login" component={LoginForm}/>
+                </Route>
             </Router>
         </ApolloProvider>
     );

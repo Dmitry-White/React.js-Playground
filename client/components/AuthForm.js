b43ae89069b83e1f4d825778e25de6e2a0cea09f
Dmitry <ecio1476@gmail.com>
2018-04-25 01:22:39 +0800

    Add displaying errors

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -35,6 +35,11 @@ class AuthForm extends Component {
                             })}
                         />
                     </div>
+
+                    <div className="errors">
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
+                    </div>
+
                     <button className="btn">Submit</button>
                 </form>
             </div>

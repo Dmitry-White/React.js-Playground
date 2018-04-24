c3088b94a46f27ade650004482cf64e6a0a47af2
Dmitry <ecio1476@gmail.com>
2018-04-24 12:36:40 +0800

    Add App and Header components

--- client/index.js
+++ client/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import {
+    Router,
+    hashHistory,
+    Route,
+    IndexRoute
+} from 'react-router';
+
+import App from './components/App';
 
 const client = new ApolloClient({
     dataIdFromObject: o => o.id
@@ -10,9 +18,9 @@ const client = new ApolloClient({
 const Root = () => {
     return (
         <ApolloProvider client={client}>
-            <div>
-              Auth Starter
-            </div>
+            <Router history={hashHistory}>
+                <Router path="/" component={App}/>
+            </Router>
         </ApolloProvider>
     );
 };

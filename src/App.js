6ea0706b36093773813ea67dca18bacb0d4fa277
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-01 21:44:21 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+        </div>
+      </BrowserRouter >
+    )
+  }
 }
 
 export default App;

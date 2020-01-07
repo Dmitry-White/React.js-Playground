b198bf4a62af3824b9ea6503fad20a447f3ec216
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-07 22:35:49 +0300

    Add articles list component

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import ArticlesList from '../components/ArticlesList';
 
 import articles from '../content/articles';
 
-const ArticlesPage = () => {
-  const getArticle = ({name, title, content}, key) => (
-    <Link className='article-list-item' key={key} to={`/articles/${name}`}>
-      <h3>{title}</h3>
-      <p>{content[0].substring(0, 150)}</p>
-    </Link>
-  )
-
-  const getArticles = (list) => list.map(getArticle);
-
-  return (
-    <>
-      <h1>Articles Page</h1>
-      {getArticles(articles)}
-    </>
-  )
-};
+const ArticlesPage = () => (
+  <>
+    <h1>Articles Page</h1>
+    <ArticlesList articles={articles} />
+  </>
+);
 
 export default ArticlesPage;

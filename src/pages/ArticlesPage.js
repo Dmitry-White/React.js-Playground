d79cd466eb79b2493cd4e38f5135a99660adb28d
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-07 22:28:32 +0300

    Add articles display functionality

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ArticlesPage = () => (
-  <>
-    <h1>Articles Page</h1>
-  </>
-);
+import articles from '../content/articles';
+
+const ArticlesPage = () => {
+  const getArticle = ({name, title, content}, key) => (
+    <Link className='article-list-item' key={key} to={`/articles/${name}`}>
+      <h3>{title}</h3>
+      <p>{content[0].substring(0, 150)}</p>
+    </Link>
+  )
+
+  const getArticles = (list) => list.map(getArticle);
+
+  return (
+    <>
+      <h1>Articles Page</h1>
+      {getArticles(articles)}
+    </>
+  )
+};
 
 export default ArticlesPage;

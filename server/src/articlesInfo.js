9842dd0a2f283e5d3ef583df45e0aa7c6b5f01f1
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-10 15:15:18 +0300

    Add basic upvote functionality

---
+++ server/src/articlesInfo.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'learn-react': {
+    upvotes: 0,
+  },
+  'learn-node': {
+    upvotes: 0,
+  },
+  'my-thoughts-on-resumes': {
+    upvotes: 0
+  }
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const articlesInfo = require('./articlesInfo');
+
 const PORT = 8000;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => res.send('Hello!'));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+
+  const article = articlesInfo[articleName];
+
+  article.upvotes += 1;
+
+  res.status(200).send(`${articleName} now has ${article.upvotes} upvotes.`);
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

deca2ba6ce12fb4ba281ccb00894cb0b09e93a4f
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-10 13:02:02 +0300

    Add body parser

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = 8000;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send('Hello!'));
 

e713c1f3b92fc08af6f8fe1432905999deaf98cb
Dmitry White <dmitry.b.white@gmail.com>
2020-01-11 17:03:56 +0300

    Add state and side-effect hooks

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 
 import ArticlesList from '../components/ArticlesList';
@@ -6,23 +6,36 @@ import ArticlesList from '../components/ArticlesList';
 import articles from '../content/articles';
 import NotFoundPage from './NotFoundPage';
 
+const defaultState = {
+  upvotes: 0,
+};
+
 const ArticlePage = () => {
   const { name } = useParams();
+
   const article = articles.find(article => article.name === name);
+  const relatedArticles = articles.filter(article => article.name !== name);
 
-  if(!article) return <NotFoundPage/>
+  const [articleInfo, setArticleInfo] = useState(defaultState);
+  useEffect(() => {
+    setArticleInfo({
+      upvotes: Math.ceil(Math.random() * 10)
+    });
+  }, [name]);
 
+  if (!article) return <NotFoundPage />
+
+  const getUpvotes = () => <p>This article has been upvoted {articleInfo.upvotes} times.</p>;
   const getParagraph = (paragraph, key) => <p key={key}>{paragraph}</p>;
   const getContent = (content) => content.map(getParagraph);
-
-  const relatedArticles = articles.filter(article => article.name !== name);
 
   return (
     <>
       <h1>{article.title}</h1>
+      {getUpvotes()}
       {getContent(article.content)}
       <h3>Related articles: </h3>
-      <ArticlesList articles={relatedArticles}/>
+      <ArticlesList articles={relatedArticles} />
     </>
   )
 };

83bb351628515b3352e4df989b20e50d6c6e8304
Dmitry White <dmitry.b.white@gmail.com>
2020-01-17 18:54:51 +0300

    Add shared list component

--- client/src/components/ArticlesList.js
+++ client/src/components/ArticlesList.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import List from './List';
+
 const ArticlesList = ({ articles }) => {
   const getArticle = ({ name, title, content }, key) => (
     <Link className='article-list-item' key={key} to={`/articles/${name}`}>
       <h3>{title}</h3>
       <p>{content[0].substring(0, 150)}</p>
     </Link>
-  )
+  );
 
-  const getArticles = (list) => list.map(getArticle);
-
-  return (
-    <>
-      <h3>Related articles: </h3>
-      {getArticles(articles)}
-    </>
-  )
+  return <List heading='Related articles' items={articles} handler={getArticle} />
 }
 
 export default ArticlesList
--- client/src/components/CommentsList.js
+++ client/src/components/CommentsList.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
+import List from './List';
+
 const CommentsList = ({ comments }) => {
   const getComment = ({ username, text }, key) => (
     <div className="comment" key={key}>
       <h4>{username}</h4>
       <p>{text}</p>
     </div>
-  )
+  );
 
-  const getComments = (list) => list.map(getComment);
-
-  return (
-    <>
-      <h3>Comments: </h3>
-      {comments.length ? getComments(comments) : <h4>No comments for this article.</h4>}
-    </>
-  )
+  return <List heading='Comments' items={comments} handler={getComment} />
 }
 
 export default CommentsList
